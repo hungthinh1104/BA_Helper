04be954b44daf6be1b5e819ec3f4ac8e3c3fa37d
Dip Hung Thinh <diphungthinh@gmail.com>
2026-06-19 20:21:15 +0700

    chore: remove junk scratch scripts

--- packages/analyzer/src/scanner/test.js
+++
@@ -1,3 +0,0 @@
-const { FrameworkDetector } = require('./framework-detector');
-const path = require('path');
-FrameworkDetector.detect(path.join(__dirname, '../../../../tests/fixtures/booking-inventory-multi-file')).then(console.log);
